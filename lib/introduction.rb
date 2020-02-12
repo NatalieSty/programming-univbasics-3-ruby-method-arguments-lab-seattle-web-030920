5e478e71fb636d219e02ca60382ff14cf092c2ba
Natalie So <nataliesotszyui@gmail.com>
2020-02-12 21:35:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,11 +5,11 @@
 
 def introduction(name)
   puts "Hi, my name is #{name}"
-}
+end
 
-def introduction_with_language(name, language){
+def introduction_with_language(name, language)
   
-}
+end
 
 
 
